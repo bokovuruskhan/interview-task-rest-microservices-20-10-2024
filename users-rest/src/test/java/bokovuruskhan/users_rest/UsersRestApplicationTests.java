69cdfa2c9a58d30f37f8a89fcaef37475aade29a
Uruskhan Bokov <bokovuruskhan@gmail.com>
2024-10-20 17:50:15 +0300

    init project

---
+++ users-rest/src/test/java/bokovuruskhan/users_rest/UsersRestApplicationTests.java
@@ -0,0 +1,13 @@
+package bokovuruskhan.users_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
